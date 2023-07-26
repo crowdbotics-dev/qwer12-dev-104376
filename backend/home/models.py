c1c7b80db0f0765a2126f776b0405abfe8067b0b
vinaysharma08 <109955878+vinaysharma08@users.noreply.github.com>
2023-07-26 16:24:47 +0530

    Update models.py

--- backend/home/models.py
+++ backend/home/models.py
@@ -6,7 +6,7 @@ class TYUI(models.Model):
     rel_ert_1_1 = models.OneToOneField("home.ERT",blank=True,null=True,on_delete=models.CASCADE,related_name="tyui_rel_ert_1_1",)
 class ERT(models.Model):
     'Generated Model'
-    rty = models.CharField(max_length=20,)
-    sda = models.BigIntegerField(null=True,blank=True,)
-    fvxv = models.BigIntegerField(null=True,blank=True,)
+    v2 = models.TextField()
+    v3 = models.TextField()
+    v4 = models.TextField()
     rel_user_1_n = models.ForeignKey("users.User",blank=True,null=True,on_delete=models.CASCADE,related_name="ert_rel_user_1_n",)
